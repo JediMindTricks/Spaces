32eac021d4717f2b235b9c4e152a6547c7f86a01
Michael <Michael.Bardwell.Scott@Gmail.com>
2017-01-03 10:21:27 -0800

    Merged styles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
  devise_for :users
  root 'home#show'
  # Added routes using resources
+ resources :users, only: [:show]
  resources :posts, only: [:new, :create]
 end
