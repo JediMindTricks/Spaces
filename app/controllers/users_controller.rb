025afa3cd842e779815ac4440d99c0f4006b2c18
Michael <Michael.Bardwell.Scott@Gmail.com>
2017-01-03 12:17:40 -0800

    Added bio and rouded images for profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,6 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:username, :avatar)
+		params.require(:user).permit(:username, :avatar, :bio)
 	end
 end
