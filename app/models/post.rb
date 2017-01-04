61e2222302db0ea60120cc23b9374f3b2c2e0bf0
Michael <Michael.Bardwell.Scott@Gmail.com>
2017-01-03 16:58:22 -0800

    Add links to user profile and home page

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,10 @@ class Post < ActiveRecord::Base
 
 	delegate :username, to: :user
 
-	self.tagged_with(name)
+	def self.tagged_with(name)
 		Tag.find_by!(name:name).posts
 	end
+	
 
 	def all_tags=(names)
 		# Names = name of tags e.g: "Music"
